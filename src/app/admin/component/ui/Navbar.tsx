2de443a8806608747c7c138f6caea121358ebf23
nadeeraka <global-nadeerakan@gmail.com>
2024-03-27 15:08:58 +0530

    init 🚀🚀🚀

--- src/app/admin/component/ui/Navbar.tsx
+++ src/app/admin/component/ui/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <nav className="max-w-full h-18 sm-h-16 container bg-black text-white">
         <ul className="  flex flex-wrap justify-between items-center py-4">
-           <Link href={''}>
+           <Link className={'bg-amber-50 text-black'} href={''}>
                Analytics
            </Link>
             <Link href={''}>
